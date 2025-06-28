e6e25475494f938a28c4c9f68307b4cfb0c4db19
briankwak810 <brian.j.kwak@gmail.com>
2025-06-28 19:14:38 +0900

    modify topomap creating code

--- deployment/src/create_topomap.sh
+++ deployment/src/create_topomap.sh
@@ -12,17 +12,20 @@ tmux splitw -h -p 50 # split it into two halves
 
 # Run roscore in the first pane
 tmux select-pane -t 0
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
 tmux send-keys "roscore" Enter
 
 # Run the create_topoplan.py script with command line args in the second pane
 tmux select-pane -t 1
-tmux send-keys "conda activate gnm_deployment" Enter
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
+tmux send-keys "conda activate vint_deployment" Enter
+tmux send-keys "cd ../visualnav-transformer/deployment/src" Enter
 tmux send-keys "python create_topomap.py --dt 1 --dir $1" Enter
 
 # Change the directory to ../topomaps/bags and run the rosbag play command in the third pane
 tmux select-pane -t 2
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
 tmux send-keys "mkdir -p ../topomaps/bags" Enter
-tmux send-keys "cd ../topomaps/bags" Enter
 tmux send-keys "rosbag play -r 1.5 $2" # feel free to change the playback rate to change the edge length in the graph
 
 # Attach to the tmux session
--- deployment/src/record_bag.sh
+++ deployment/src/record_bag.sh
@@ -12,17 +12,21 @@ tmux splitw -h -p 50 # split it into two halves
 
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
-tmux send-keys "roslaunch vint_locobot.launch" Enter
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
+tmux send-keys "roscore" Enter
 
 # Run the teleop.py script in the second pane
 tmux select-pane -t 1
-tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
+tmux send-keys "export MAGNUM_CPU_RUNTIME_DETECT=OFF" Enter
+tmux send-keys "export HABITAT_SIM_LOG=quiet" Enter
+tmux send-keys "export MAGNUM_LOG=quiet" Enter
+tmux send-keys "python ../ros_x_habitat/src/scripts/roam_with_keyboard.py" Enter
 
 # Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
 tmux select-pane -t 2
-tmux send-keys "cd ../topomaps/bags" Enter
-tmux send-keys "rosbag record /usb_cam/image_raw -o $1" # change topic if necessary
+tmux send-keys "source ~/setup_vintxhab.sh" Enter
+tmux send-keys "rosbag record /rgb -o $1" # change topic if necessary
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
